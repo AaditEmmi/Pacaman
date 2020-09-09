4b5719a837014e33f9214e4442a2a833955a0936
AaditEmmi <62754753+AaditEmmi@users.noreply.github.com>
2020-09-09 15:59:59 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,10 @@
-var Pacman, Football, Ghost1, Ghost2, Ghost3;
+var Pacman,  Ghost1, Ghost2, Ghost3;
 var PacmanImage, FootballImage, Ghost1Image, Ghost2Image , Ghost3Image ;
+var GameState;
+var PlayState = 1;
+var ENDState = 0;
+GameState = PlayState;
+
 
 function preload(){
 PacmanImage = loadImage("images/Pacman(2).jpg");
@@ -14,8 +19,6 @@ function setup() {
    createCanvas(800,400);
    Pacman = createSprite(125,140,80,90);
    Pacman.addImage(PacmanImage);
-   Football = createSprite(135,145,60,50);
-   Football.addImage(FootballImage);
    Ghost1 = createSprite(125,135,60,80);
    Ghost1.addImage(Ghost1Image);
    Ghost2 = createSprite(155,165,70,80);
@@ -27,9 +30,34 @@ function setup() {
 
 function draw() {
   background(255,255,255);  
+
+  Ghost1.velocityX = Math.round(random(-3,3));
+  Ghost2.velocityX = Math.round(random(-3.3));
+  Ghost3.velocityX = Math.round(random(-3,3));
+
+  if ( GhostsGroup. isTouching (Pacman)){
+       GameState = END;
+  }
+    
   drawSprites();
+
 }
 
-function spawndots(){
-  
+function spawnFootballs(){
+  if (frameCount % 60 === 0){
+   var Football1 = createSprite(400,100,60,50);
+    Football1.addImage(FootballImage);
+    Football1.velocityX = -3; 
+    
+    var Football2 = createSprite(450,200,60,50);
+    Football2.addImage(FootballImage);
+    Football2.velocityX = -3;
+
+    var Football3 = createSprite(500,300,60,50);
+    Football2.addImage(FootballImage);
+    Football3.velocityX = -3;
+
+  }
 }
+
+
